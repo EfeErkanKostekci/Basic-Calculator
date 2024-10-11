664463a54b481e77c5b73aa8e1820089cdf96ebc
Erkan <108893420+EfeErkanKostekci@users.noreply.github.com>
2024-10-11 20:15:35 +0300

    Add files via upload

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.Write("Choose a operation (addition, subtraction, multiplication, division): ");
+String input = Console.ReadLine();
+
+Console.Write("Enter the first number: ");
+String num1String = Console.ReadLine();
+
+Console.Write("Enter the second number: ");
+String num2String = Console.ReadLine();
+
+int num1, num2;
+int.TryParse(num1String, out num1);
+int.TryParse(num2String, out num2);
+
+switch (input)
+{
+    case "addition":
+        Console.WriteLine("The sum of the numbers you entered: " + (num1 + num2));
+        break;
+
+    case "subtraction":
+        Console.WriteLine("The difference between the numbers you entered: " + (num1 - num2));
+        break;
+
+    case "multiplication":
+        Console.WriteLine("The product of the two numbers you entered: " + (num1 * num2));
+        break;
+
+    case "division":
+        Console.WriteLine("The quotient of the two numbers you entered: " + (num1 / num2));
+        break;
+
+    default:
+        Console.WriteLine("You entered a incorrect value!");
+        break;
+}
